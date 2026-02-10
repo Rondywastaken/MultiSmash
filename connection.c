cbf4bf899bb4bddc489879ddc50c93f89906d800
Roni Temizsoy <Itzrondy@gmail.com>
2026-02-10 13:15:17 +0100

    changed ip

--- connection.c
+++ connection.c
@@ -85,7 +85,7 @@ void *client_thread(void *arg) {
   server_addr.sin_port = htons(8080);
 
   //inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
-  inet_pton(AF_INET, "10.179.255.109", &server_addr.sin_addr);
+  inet_pton(AF_INET, "10.146.195.134", &server_addr.sin_addr);
 
   socklen_t client_addr_len = sizeof(client_addr);
   
