67bb87cb10d6dd8f05eeb528b9a6bfbb126ca505
Roni Temizsoy <Itzrondy@gmail.com>
2026-02-10 10:57:51 +0100

    Changed static ip

--- connection.c
+++ connection.c
@@ -85,7 +85,7 @@ void *client_thread(void *arg) {
   server_addr.sin_port = htons(8080);
 
   //inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
-  inet_pton(AF_INET, "192.168.0.134", &server_addr.sin_addr);
+  inet_pton(AF_INET, "10.179.255.109", &server_addr.sin_addr);
 
   socklen_t client_addr_len = sizeof(client_addr);
   
